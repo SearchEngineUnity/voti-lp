d9ded46e2e64ab90334e93296108ed055cc44f44
holly-searchengineunity <holly@searchengineunity.com>
2021-01-26 12:55:12 -0500

    cta Segment completed

--- web/src/components/CtaSegment.js
+++ web/src/components/CtaSegment.js
@@ -2,21 +2,18 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 // import styled from 'styled-components';
 
-function CtaSegment() {
+function CtaSegment(ctaText, buttonText, buttonIcon, buttonLink, id) {
   return (
-    <section className="p-5" style={{ background: '#3c6286' }}>
+    <section id={id} className="p-5" style={{ background: '#3c6286' }}>
       <Container>
         <Row>
           <Col className="col-md-6 col-12" style={{ color: 'white' }}>
-            <p>
-              Interested in <br /> more Information?
-            </p>
+            <p ctaText={ctaText} />
           </Col>
-
           <Col className="col-md-6 col-12 text-center font-weight-light text-dark">
-            <a href="https://votidetection.com/contact-us/" className="d-table table-hover">
-              <span className="p-2 border">T</span>
-              <p className="p-2 d-inline border">CONTACT US</p>
+            <a href={buttonLink} className="d-table table-hover">
+              <span buttonIcon={buttonIcon} className="p-2 border" />
+              <p buttonText={buttonText} className="p-2 d-inline border" />
             </a>
           </Col>
         </Row>
