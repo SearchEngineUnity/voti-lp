1ff5d11bb9e751a74a3fa996736a37743f2b05a7
dev-searchengineunity <67386875+dev-searchengineunity@users.noreply.github.com>
2021-02-01 09:08:29 -0500

    Netlify build button code (#35)
    Co-authored-by: holly-searchengineunity <holly@searchengineunity.com>

---
+++ studio/src/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+    {
+      name: 'netlify',
+      options: {
+        title: 'My Netlify deploys',
+        sites: [
+          {
+            title: 'Voti Page Builder',
+            apiId: '53a2d6bd-346b-497f-a574-dbe277f81f72',
+            buildHookId: '601303c2ebf8ff262ecb2075',
+            name: 'voti-lp',
+          },
+        ],
+      },
+    },
+  ],
+};
